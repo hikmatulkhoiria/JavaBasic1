ea4fcea146eba5f9670ed804fbb5ce1880a0362d
Hikmatul Khoiria <hikmatulkhoiria@gmail.com>
2021-10-12 19:18:37 +0700

    initial first

--- BasicJava.java
+++ BasicJava.java
@@ -3,20 +3,20 @@ public class BasicJava {
         String firstName = "Hikmatul ";
         String lastName = "Khoiria";
         String address = "Medan Johor ";
-        String numberPhone = "081312345678 ";
+        String phoneNumber = "081312345678 ";
         int age = 21 ;
         String hobby = "Drakoran ";
         String favoriteFood = "Masakan Mama ";
         double dollar = 99.99 ;
         boolean single = true ;
-            System.out.println(firstName + lastName);
-            System.out.println(address);
-            System.out.println(numberPhone);
-            System.out.println(age);
-            System.out.println(hobby);
-            System.out.println(favoriteFood);
-            System.out.println(dollar);
-            System.out.println(single);
+            System.out.println("Full name       : " + firstName + lastName);
+            System.out.println("Address         : " + address);
+            System.out.println("Phone Number    : " + phoneNumber);
+            System.out.println("Age             : " + age);
+            System.out.println("Hobby           : " + hobby);
+            System.out.println("FavoriteFood    : " + favoriteFood);
+            System.out.println("Dollar          : " + dollar);
+            System.out.println("Single          : " + single);
 
     }
 }
