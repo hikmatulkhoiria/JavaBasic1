2b171124ab5108995d9d86e6481d84632664d9b1
Hikmatul Khoiria <hikmatulkhoiria@gmail.com>
2021-10-12 12:23:24 +0700

    first commit

---
+++ BasicJava.java
@@ -0,0 +1,22 @@
+public class BasicJava {
+ public static void main(String[] args) {
+        String firstName = "Hikmatul ";
+        String lastName = "Khoiria";
+        String address = "Medan Johor ";
+        String numberPhone = "081312345678 ";
+        int age = 21 ;
+        String hobby = "Drakoran ";
+        String favoriteFood = "Masakan Mama ";
+        double dollar = 99.99 ;
+        boolean single = true ;
+            System.out.println(firstName + lastName);
+            System.out.println(address);
+            System.out.println(numberPhone);
+            System.out.println(age);
+            System.out.println(hobby);
+            System.out.println(favoriteFood);
+            System.out.println(dollar);
+            System.out.println(single);
+
+    }
+}
